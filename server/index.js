05740b01f92f9d9415c36c1c2d669e8499fa61d8
Muhammad Asif Javed <axif.javed@gmail.com>
2019-02-04 12:46:33 +0500

    webpack middleware moved to index

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.HTTP_PORT = process.env.HTTP_PORT || 3000;
+
 const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
-const setupApiRoutes = require('./middlewares/api');
 const logger = require('./logger');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-process.env.HTTP_PORT = process.env.HTTP_PORT || 3000;
+const { resolve } = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+
+const app = express();
+
 
 function onUnhandledError(err) {
   try {
@@ -20,20 +28,31 @@ function onUnhandledError(err) {
 process.on('unhandledRejection', onUnhandledError);
 process.on('uncaughtException', onUnhandledError);
 
-const setupAppRoutes =
-  process.env.NODE_ENV === 'development' ? require('./middlewares/development') : require('./middlewares/production');
+var isDevelopment = process.env.NODE_ENV === 'development';
 
-const app = express();
+if (isDevelopment) {
+  const webpackConfig = require('../webpack.config.dev');
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, { logger, publicPath: webpackConfig.output.publicPath, stats: { colors: true } }));
+  app.use(webpackHotMiddleware(compiler));
+  // all other requests be handled by UI itself
+  app.get('*', (req, res) => res.sendFile(resolve(__dirname, '..', 'build-dev', 'client', 'index.html')));
+
+} else {
+  const clientBuildPath = resolve(__dirname, 'client');
+
+  app.use(compression());
+  app.use('/', express.static(clientBuildPath));
+
+  // all other requests be handled by UI itself
+  app.get('*', (req, res) => res.sendFile(resolve(clientBuildPath, 'index.html')));
+}
 
 app.set('env', process.env.NODE_ENV);
 logger.info(`Application env: ${process.env.NODE_ENV}`);
 
 app.use(logger.expressMiddleware);
 app.use(bodyParser.json());
-
-// application routes
-setupApiRoutes(app);
-setupAppRoutes(app);
 
 http.createServer(app).listen(process.env.HTTP_PORT, () => {
   logger.info(`HTTP server is now running on http://localhost:${process.env.HTTP_PORT}`);
